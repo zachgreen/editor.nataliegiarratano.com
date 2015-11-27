1370d3afb9fa45844d7d748c980fd6c9c2ea2ad8
Zach Green <zach.green@gmail.com>
2015-11-27 14:39:37 -0500

    More nav

---
+++ routes.rb
@@ -0,0 +1,4 @@
+get '/', :to => redirect('/index.html')
+get '/index', :to => redirect('/index.html')
+get '/resume', :to => redirect('/resume.html')
+get '/contact', :to => redirect('/contact.html')
